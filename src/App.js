53a1b9939d26e49f54bf16a3b4739fd18eec7f8e
kartikeya532001 <kartikeya532001@gmail.com>
2024-09-04 22:30:11 +0530

    Changes to the code made

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './ORI.png';
 import './App.css';
 import Customers from './Customers'
 import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
@@ -14,7 +14,7 @@ class App extends Component {
         <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Simple React App</h1>
+          <h1 className="App-title">Oriserve</h1>
         </header>
           <Switch>
                 <Route exact path= "/" render={() => (
